7622ef6a41c9bf0beb99793fe1a64c4aca8ef133
dean joseph <deanjoseph3@hotmail.com>
2018-04-02 11:53:42 +0000

    updated node_modules and worked on index/scss/js

--- build/js/js.js
+++ build/js/js.js
@@ -1,16 +1,32 @@
-/*
- * @name :
- * @desc:
+/**
+ * @file    js/js.js
+ * @desc    this will use jQuery and enquire to handle navigation
  */
-
+// IIFE, read as iffy
 ( () => {
+    let
+        /*$               =   require("node_modules/jquery/dist/jquery.min"),
+        enquire         =   require("node_modules/enquire.js/dist/enquire.min"),*/
+        $menuBtn        =   $(".menuBtn"),
+        $navContainer   =   $(".topNav .container")
+    ;
 
+    window.addEventListener("load", () => {
+        enquire.register("screen and (min-width: 768px)", {
 
+            match: function () {
+                $navContainer.show();
+            },
 
+            unmatch: function () {
+                $navContainer.hide();
+            }
+        }); // enquire
 
-    init = () => {
+        $menuBtn.on("click", () => {
+            $navContainer
+                .slideToggle();
+        })// click
+    });
 
-    }; //init
-
-window.addEventListener("load", init);
 })();
